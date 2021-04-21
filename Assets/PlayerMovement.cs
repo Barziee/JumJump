8ff4c055e0853c06b5aee1329a3651e9b3ed1178
Bar Ronen <barzie02@gmail.com>
2021-04-21 16:34:07 +0300

    PlayerMovement Logic

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public float moveInput;
-    public bool isGrounded;
-
     private Rigidbody2D rb;
     public LayerMask groundMask;
 
+    public float moveSpeed;
+    public float moveInput;
+    [SerializeField] private bool isGrounded;
+
     public bool canJump = true;
-    public float jumpValue = 0.0f;
+    public float jumpValue = 0f;
 
 
     void Start()
@@ -24,11 +22,14 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
         isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f),
         new Vector2(0.9f, 0.4f), 0f, groundMask);
 
+        JumpLogic();
+    }
+
+    private void JumpLogic()
+    {
         if (Input.GetKey("space") && isGrounded && canJump)
         {
             jumpValue += 0.1f;
@@ -39,8 +40,35 @@ public class PlayerMovement : MonoBehaviour
             float tempX = moveInput * moveSpeed;
             float tempY = jumpValue;
             rb.velocity = new Vector2(tempX, tempY);
+            Invoke("JumpReset", 0.2f);
         }
 
+        if (Input.GetKeyUp("space"))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(moveInput * moveSpeed, jumpValue);
+                jumpValue = 0f;
+            }
+            canJump = true;
+        }
+
+        if (Input.GetKeyDown("space") && isGrounded && canJump)
+        {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+
+        if (jumpValue == 0f && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        }
+    }
+
+    private void JumpReset()
+    {
+        canJump = false;
+        jumpValue = 0;
     }
 
     private void OnDrawGizmosSelected()
