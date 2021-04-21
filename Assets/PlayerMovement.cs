a69989fcd88ad4ba189a4ee61274ec52944416d9
Bar Ronen <barzie02@gmail.com>
2021-04-21 16:16:54 +0300

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveInput;
+    public bool isGrounded;
+
+    private Rigidbody2D rb;
+    public LayerMask groundMask;
+
+    public bool canJump = true;
+    public float jumpValue = 0.0f;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f),
+        new Vector2(0.9f, 0.4f), 0f, groundMask);
+
+        if (Input.GetKey("space") && isGrounded && canJump)
+        {
+            jumpValue += 0.1f;
+        }
+
+        if (jumpValue >= 20f && isGrounded)
+        {
+            float tempX = moveInput * moveSpeed;
+            float tempY = jumpValue;
+            rb.velocity = new Vector2(tempX, tempY);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.5f), new Vector2(0.9f, 0.2f));
+    }
+
+}
